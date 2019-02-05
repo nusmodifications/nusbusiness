629885f6e4bc6fdc5453ad3a8ecf638246d9571a
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-02-05 09:54:18 +0800

    Split map into its own class

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import "normalize.css";
+import "leaflet/dist/leaflet.css";
+
+import "../styles.scss";
+
+import ToiletMap from "./map/map"
 
 document.getElementById("locate")
   .addEventListener('click', (evt) => {
     evt.preventDefault();
 
-    navigator.geolocation.getCurrentPosition(function (position) {
-      window.location = `map?lat=${position.coords.latitude}&lng=${position.coords.longitude}`
+    navigator.geolocation.getCurrentPosition((position) => {
+      history.pushState(null, 'NUS Business', `map?lat=${position.coords.latitude}&lng=${position.coords.longitude}`);
+      // Fade out overlay
+      const overlay = document.querySelector('.intro-overlay');
+      overlay.classList.add('fade');
+      setTimeout(() => { overlay.classList.add('hide') }, 300);
     });
   });
+
+const map = new ToiletMap('map');
