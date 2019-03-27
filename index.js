c663b139c5e89ed7379c1cf8c84050e6e16f3b45
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-03-27 11:25:46 +0800

    Add results and scaffold filters

--- index.js
+++ index.js
@@ -5,22 +5,28 @@ import "./styles.scss";
 
 import ToiletMap from "./map/map";
 
+const map = new ToiletMap("map", "list");
+
 document.getElementById("locate").addEventListener("click", evt => {
   evt.preventDefault();
 
   navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+
     history.pushState(
       null,
       "NUS Business",
-      `map?lat=${position.coords.latitude}&lng=${position.coords.longitude}`
+      `map?lat=${latitude}&lng=${longitude}`
     );
+
     // Fade out overlay
     const overlay = document.querySelector(".intro-overlay");
     overlay.classList.add("fade");
     setTimeout(() => {
       overlay.classList.add("hide");
     }, 300);
+
+    // Pan map to current location
+    map.setLocation(latitude, longitude);
   });
 });
-
-const map = new ToiletMap("map");
---
+++ map/result.js
@@ -0,0 +1,7 @@
+export class Result {
+  constructor(container, location, cluster) {
+    this.toilets = cluster;
+    this.container = container;
+    this.location = location;
+  }
+}
